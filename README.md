8b8c5dc010f3e5b134da61db1c26eafc73f661bc
joseulisses065 <sulisses111@gmail.com>
2024-05-13 11:49:14 -0300

    task: add documentation swagger and readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# AnimeApi
-anime api with java and framework spring boot
+# AnimeAPI
+
+![](https://img.shields.io/github/license/alura-cursos/android-com-kotlin-personalizando-ui)
+[![GitHub Release](https://img.shields.io/github/release/lihengming/spring-boot-api-project-seed.svg)](https://github.com/Joseulisses065/AnimeApi/releases )
+
+# Resume
+
+An anime API project that lists different animes with data such as category description title and where to watch
+
+## 🔨 Project features
+
+The Api makes it possible to query by id as well as retrieve the complete list of animes, making it possible to insert and update as well as delete animes.
+
+![](img/amostra.gif)
+
+## ✔️ Techniques and technologies used
+
+The techniques and technologies used for this are:
+
+- `Java 17`: Latest long-term support (LTS) version of the Java platform
+- `Spring Boot 3`: A framework that facilitates the creation of Spring-based Java applications, offering automatic configuration and sober configuration opinions.
+- `PostgreSQL`: An object-relational database management system, developed as an open source project.
+- `Docker`: A set of platform-as-a-service products that use operating system-level virtualization to deliver software in packages called containers.
+
+
+
+## 🎯 Challenge
+
+Product details screen
+
+![orgs-desafio-details](./img/api_swegger.png)
+
+
+
+## 📁 Access to the project
+
+You can [access the source code of the initial project](https://github.com/Joseulisses065/AnimeApi)
+
+## 🛠️ Open and run the project
+### Clone this repository
+```
+git clone
+```
--- src/main/java/br/com/animeapi/controllers/AnimeController.java
+++ src/main/java/br/com/animeapi/controllers/AnimeController.java
@@ -4,6 +4,10 @@ import br.com.animeapi.domain.dto.request.AnimeRequestDto;
 import br.com.animeapi.domain.dto.request.AnimeRequestUpdateDto;
 import br.com.animeapi.domain.dto.response.AnimeResponseDto;
 import br.com.animeapi.services.AnimeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,12 +22,18 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/animes")
+@Tag(name = "AnimeApi")
 public class AnimeController {
 
 
     @Autowired
     private AnimeService animeService;
 
+    @Operation(summary = "Add new animes", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Inserted successfully")
+
+    })
     @PostMapping
     public ResponseEntity<Void> createAnime(@RequestBody @Valid AnimeRequestDto anime, UriComponentsBuilder builder) {
         AnimeResponseDto response = animeService.createAnime(anime);
@@ -33,24 +43,46 @@ public class AnimeController {
 
     }
 
+    @Operation(summary = "Find all animes", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping
     public ResponseEntity<Page<AnimeResponseDto>> findAll(Pageable page) {
         var response = animeService.getAnimes(page);
         return ResponseEntity.ok(response);
     }
 
+
+    @Operation(summary = "Find anime by id", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<AnimeResponseDto> findById(@PathVariable UUID id) {
         var response = animeService.findById(id);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Update anime by id", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @PutMapping("/{id}")
     public  ResponseEntity<Void> updateAnime(@PathVariable UUID id, @RequestBody AnimeRequestUpdateDto anime){
         animeService.updateAnime(id,anime);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete anime by id", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAnime(@PathVariable UUID id){
         animeService.deleteAnime(id);
